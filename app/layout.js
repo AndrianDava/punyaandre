3daed1eb9808089457d5d56225ea32b086951850
Andrian <111829852+AndrianDava@users.noreply.github.com>
2024-03-19 20:35:17 +0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -17,7 +17,7 @@ export default function RootLayout({ children }) {
             <head>
                 <link rel='icon' href='/favicon.ico' />
             </head>
-            <body className={inter.className} style={{ backgroundColor: '#323232' }}>{children}</body>
+            <body className={inter.className} style={{ backgroundColor: '#ACE2E1' }}>{children}</body>
         </html>
     )
 }
