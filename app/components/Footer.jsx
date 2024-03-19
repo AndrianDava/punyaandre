7ae7ca3d5454d49617d6a1576c5d3b5b54588b7d
Andrian <111829852+AndrianDava@users.noreply.github.com>
2024-03-19 18:55:25 +0700

    Update Footer.jsx

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -7,7 +7,7 @@ export default class Footer extends Component {
             <Grid.Row>
                 <Grid.Column width={12}>
                     <Message color={'grey'}>
-                        <b>Made with <Icon name='heart' color='red' fitted /> by <a href="https://gabrieltavares.dev" style={{ textDecoration: 'none' }}>Gabriel Tavares</a></b>
+                        <b>Dibuat Oleh <a>Andrian</a></b>
                     </Message>
                 </Grid.Column>
             </Grid.Row>
