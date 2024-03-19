6cdc37d252edc389ff3631d938aed0a92a38d941
Andrian <111829852+AndrianDava@users.noreply.github.com>
2024-03-19 19:27:35 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ const nextConfig = {
                 source: "/api/:path*",
                 headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "https://andzz.dev" },
+                    { key: "Access-Control-Allow-Origin", value: "https://yt-downloader-tawny.vercel.app/" },
                     { key: "Access-Control-Allow-Methods", value: "POST" },
                     { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
                 ]
