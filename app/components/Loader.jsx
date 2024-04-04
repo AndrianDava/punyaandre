72f575daa0d535918919a96c8e0ee0cb55427838
Andrian <111829852+AndrianDava@users.noreply.github.com>
2024-04-04 20:41:46 +0700

    Update Loader.jsx

--- app/components/Loader.jsx
+++ app/components/Loader.jsx
@@ -4,7 +4,7 @@ import { Grid, Dimmer, Loader as SemanticLoader } from 'semantic-ui-react'
 export default class Loader extends Component {
     render() {
         return (
-            <Grid textAlign='center' style={{ height: '100%', alignContent: 'center', backgroundColor: '#323232' }}>
+            <Grid textAlign='center' style={{ height: '100%', alignContent: 'center', backgroundColor: '#31363F' }}>
                 <Dimmer active>
                     <SemanticLoader />
                 </Dimmer>
