f4d306120c36d01c487c4b20e1bf04040d6d14bd
Andrian <111829852+AndrianDava@users.noreply.github.com>
2024-04-18 18:38:51 +0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -15,7 +15,8 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <head>
-      <script async custom-element="amp-ad" src="https://cdn.ampproject.org/v0/amp-ad-0.1.js"></script>
+     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8637881957634572"
+     crossorigin="anonymous"></script>
             </head>
             <body className={inter.className} style={{ backgroundColor: '#222831' }}>{children}</body>
         </html>
